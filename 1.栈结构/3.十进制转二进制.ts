a395c6c5422898c3b1ae16a14084cf5420fac170
胡斌 <hubin@weifengqi18.com>
2023-03-09 20:38:57 +0800

    fix: number to bit

---
+++ 1.栈结构/3.十进制转二进制.ts
@@ -0,0 +1,38 @@
+import { ArrayStack } from "./1.ArrayStack";
+
+// 十进制to二进制数学题算法?
+/*
+// 先进行 %
+// 对bit不断 / 2
+  35 1
+  17 1
+  8 0
+  4 0
+  2 0
+  1 1
+
+
+*/
+const numberToBit = (bit: number): string => {
+  const stack = new ArrayStack<number>();
+  let str = "";
+
+  while (bit > 1) {
+    stack.push(bit % 2);
+    bit = Math.floor(bit / 2);
+  }
+
+  console.log(stack);
+
+  return str;
+};
+
+numberToBit(35);
+
+/*
+  
+  1 0 0 0 1 1
+
+  32 16 8 4 2 1
+
+*/
