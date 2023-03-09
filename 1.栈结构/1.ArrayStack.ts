4e8ec2b9b64925256d3c49f8932be57fa9a88da2
胡斌 <hubin@weifengqi18.com>
2023-03-09 19:38:29 +0800

    arrayStack

---
+++ 1.栈结构/1.ArrayStack.ts
@@ -0,0 +1,32 @@
+import type { IStack } from "./IStack";
+
+class ArrayStack<T> implements IStack<T> {
+  private data: T[] = [];
+  push(element: T): void {
+    this.data.push(element);
+  }
+  pop(): T | undefined {
+    return this.data.pop();
+  }
+  peek(): T | undefined {
+    return this.data[this.data.length - 1];
+  }
+  size(): Number {
+    return this.data.length;
+  }
+  isEmpty(): boolean {
+    return this.data.length === 0;
+  }
+}
+
+const stack = new ArrayStack<string>();
+// stack.push("ice");
+// stack.push("panda");
+// stack.push("quezz");
+
+// console.log(stack.peek());
+// console.log(stack.size());
+// console.log(stack.pop());
+// console.log(stack.pop());
+// console.log(stack.isEmpty());
+// console.log(stack.peek());
---
+++ 1.栈结构/IStack.ts
@@ -0,0 +1,12 @@
+export interface IStack<T> {
+  // 入栈
+  push(element: T): void;
+  // 出栈
+  pop(): T | undefined;
+  // 查看栈顶元素
+  peek(): T | undefined;
+  // 元素个数
+  size(): Number;
+  // 是否为空
+  isEmpty(): boolean;
+}
