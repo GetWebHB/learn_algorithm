db3b2093f11471ba24e90d841722d4e9bf0d2eaa
GetWebHB <1597834867@qq.com>
2023-03-12 21:12:47 +0800

    fix: change types

--- 2.队列/IQueue.ts
+++ 2.队列/IQueue.ts
@@ -1,7 +1,7 @@
 export interface IQueue<T> {
-  enqueue: (ele) => void;
-  dequeue: () => T | undefined;
-  front: () => T | undefined;
-  isEmpty: () => boolean;
-  size: () => number;
+  enqueue(ele: T): void;
+  dequeue(): T | undefined;
+  front(): T | undefined;
+  isEmpty(): boolean;
+  size(): number;
 }
